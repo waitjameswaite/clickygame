6c567970fd91e95c7f6f0c8578b716ed71d1fc10
James Waite <James Waite>
2017-12-05 18:41:16 -0500

    updated README

--- src/App.js
+++ src/App.js
@@ -1,17 +1,16 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
+        <nav className="navbar navbar-default">
+          <h1 className="App-title">Clicky Game</h1>
+        </nav>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+        <p className="App-description">
         </p>
       </div>
     );
